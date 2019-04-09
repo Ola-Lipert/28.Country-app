c7d1a159df9197125d843dc6cc97a65cb0e395cd
Aleksandra Lipert <ola.luszcz@interia.pl>
2019-04-09 23:51:58 +0200

    Add Provider and redux-devtools

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import DevTools from './DevTools';
 
 render(
-    <h1>Inicjalizacja projektu</h1>,
+    <Provider store={store}>
+        <h1>Inicjalizacja projektu</h1>
+        <DevTools />
+    </Provider>,
     document.getElementById('root')
 );
