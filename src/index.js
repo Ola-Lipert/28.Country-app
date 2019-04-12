6e04525cf7fbb3be71a395ab464fdc2a2e0ea4d6
Aleksandra Lipert <ola.luszcz@interia.pl>
2019-04-13 00:04:13 +0200

    Add components: home, contact and not-found

--- src/index.js
+++ src/index.js
@@ -3,16 +3,14 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import DevTools from './DevTools';
-import { getCountries } from './actions/actions-countries';
+//import { getCountries } from './actions/actions-countries';
 
 render(
     <Provider store={store}>
-        <div>
-            <h1>Inicjalizacja projektu</h1>
-            <DevTools />
-        </div>
+        <Router history={hashHistory} routes={routes}/>
+        <DevTools/>
     </Provider>,
     document.getElementById('root')
 );
 
-store.dispatch(getCountries());
+//store.dispatch(getCountries());
